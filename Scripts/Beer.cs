c2fdb634f22b2b35682a84f4135ba11ad792e81e
tarunja1ks <tarunjaiks@gmail.com>
2026-02-22 10:26:26 -0800

    beer

--- Scripts/Beer.cs
+++ Scripts/Beer.cs
@@ -1,19 +1,25 @@
 using Godot;
 using System;
 
-public partial class Beerbottle : Area2D
+public partial class Beer : Area2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
-		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
-		
 	}
+	
+	public override void _on_body_entered(Node2d body){
+		if(body is Player){
+			body.TakeDamage(10);
+		}
+	}
+	public override void _on_mouse_entered(){
+		// make the beer bottle cancel
+	}
+	
 }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -17,60 +17,59 @@ public partial class Player : CharacterBody2D
 		MoveAndCollide(Velocity * (float)delta);
 	}
 
-    private int _speed = 300;
-    private double health = 100;
+	private int _speed = 300;
+	private double health = 100;
 
 
-    private RichTextLabel healthText;
+	private RichTextLabel healthText;
 
-    public override void _Ready()
+	public override void _Ready()
 	{
-        healthText = GetNode<RichTextLabel>("HealthText");
-        UpdateHealthText();
+		healthText = GetNode<RichTextLabel>("HealthText");
+		UpdateHealthText();
 	}
 
-    public void GetInput()
-    {
-        // Vector2 inputDir = Input.GetVector("MoveLeft", "MoveRight", "null", "null");
-        // Input.ge
-        Vector2 direction = Vector2.Zero;
-        if(Input.IsActionJustPressed("MoveLeft"))
-        {
-            direction = Vector2.Left;
-        }
+	public void GetInput()
+	{
+		// Vector2 inputDir = Input.GetVector("MoveLeft", "MoveRight", "null", "null");
+		// Input.ge
+		Vector2 direction = Vector2.Zero;
+		if(Input.IsActionJustPressed("MoveLeft"))
+		{
+			direction = Vector2.Left;
+		}
 
-        if(Input.IsActionJustPressed("MoveRight"))
-        {
-            direction = Vector2.Right;
-        }
-        
-        Velocity = direction * _speed;
-    }
+		if(Input.IsActionJustPressed("MoveRight"))
+		{
+			direction = Vector2.Right;
+		}
+		
+		Velocity = direction * _speed;
+	}
 
-    
+	
 
-    public override void _PhysicsProcess(double delta)
-    {
-        GetInput();
-        // MoveAndCollide(Velocity * (float)delta);
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		GetInput();
+		// MoveAndCollide(Velocity * (float)delta);
+	}
 
-    public double GetHealth()
-    {
-        return health;
-    }
+	public double GetHealth()
+	{
+		return health;
+	}
 
-    public double TakeDamage(double damage)
-    {
-        health -= damage;
-        UpdateHealthText();
-        return health;
-    }
+	public double TakeDamage(double damage)
+	{
+		health -= damage;
+		UpdateHealthText();
+		return health;
+	}
 
 
-    private void UpdateHealthText()
-    {
-        healthText.Text = "Health: " + GetHealth();
-    }
+	private void UpdateHealthText()
+	{
+		healthText.Text = "Health: " + GetHealth();
+	}
 }
-
